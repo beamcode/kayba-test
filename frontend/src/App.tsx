e6c81a1a243aa99a03f6de3876bb2a4aa298da6c
Mac <samuel.palmer@epitech.eu>
2025-03-15 07:39:11 +0100

    [*] add / redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 import Todos from "@/pages/Todos"
 import SignUp from "@/pages/SignUp"
 import SignIn from "@/pages/SignIn"
 
 function App() {
+  // didnt do protected routes but i could have done
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
         <Route path="/todos" element={<Todos />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
